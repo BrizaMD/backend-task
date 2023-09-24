0eca38a73c148407e9e506de818608dad3207551
GEOMANT\fjancsar <fjancsar@geomant.com>
2023-09-24 19:21:27 +0200

    Created file parser

---
+++ TruckOrderOptimizer/Helpers/FileParser.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.IO;
+using TruckOrderOptimizer.Models;
+
+namespace TruckOrderOptimizer.Helpers
+{
+    public static class FileParser
+    {
+        public static void ParseTrucksAndJobs(string filepath, out List<Vehicle> vehicles, out List<Job> jobs)
+        {
+            vehicles = new List<Vehicle>();
+            jobs = new List<Job>();
+
+            using (var reader = new StreamReader(filepath))
+            {
+                int vehicleCount = int.Parse(reader.ReadLine());
+                for (int i = 0; i < vehicleCount; i++)
+                {
+                    string[] tokens = reader.ReadLine().Split(' ');
+                    int vehicleId = int.Parse(tokens[0]);
+                    var compatibleJobs = new List<string>(tokens).GetRange(1, tokens.Length - 1);
+                    vehicles.Add(new Vehicle { Id = vehicleId, CompatibleJobs = compatibleJobs });
+                }
+
+                int jobCount = int.Parse(reader.ReadLine());
+                for (int i = 0; i < jobCount; i++)
+                {
+                    string[] tokens = reader.ReadLine().Split(' ');
+                    int jobId = int.Parse(tokens[0]);
+                    string jobType = tokens[1];
+                    jobs.Add(new Job { Id = jobId, Type = jobType });
+                }
+            }
+        }
+    }
+}
