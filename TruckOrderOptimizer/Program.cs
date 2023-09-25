dd7c70d11d9e18ed6da9083e8de31c2a12926484
GEOMANT\fjancsar <fjancsar@geomant.com>
2023-09-25 20:23:07 +0200

    Put logging and output writing to its own class, removed unused imports

--- TruckOrderOptimizer/Program.cs
+++ TruckOrderOptimizer/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using TruckOrderOptimizer.Helpers;
 using TruckOrderOptimizer.Services;
 using Serilog;
-using TruckOrderOptimizer.Models;
 
 namespace TruckOrderOptimizer
 {
@@ -20,21 +17,28 @@ namespace TruckOrderOptimizer
             {
                 var filePath = ResolveFilePath("mediordev.txt");
 
-                // Start the stopwatch
                 var stopwatch = Stopwatch.StartNew();
 
                 var (vehicles, jobs) = FileParser.ParseTrucksAndJobs(filePath);
                 var (assignments, unassignedJobs, unassignedVehicles) = JobAssignmentService.AssignJobs(vehicles, jobs);
 
-                // Stop the stopwatch
                 stopwatch.Stop();
 
-                // Log the elapsed time
                 Log.Information("Elapsed time: {ElapsedMilliseconds} ms", stopwatch.ElapsedMilliseconds);
 
-                // PrintAssignments(assignments, vehicles, jobs);
-                // PrintUnassignedJobs(unassignedJobs);
-                // PrintUnassignedVehicles(unassignedVehicles);
+                AssignmentReporter.PrintAssignments(assignments, vehicles, jobs);
+                AssignmentReporter.PrintUnassignedJobs(unassignedJobs);
+                AssignmentReporter.PrintUnassignedVehicles(unassignedVehicles);
+
+                string timeStamp = DateTime.Now.ToString("HH-mm-ss-fff");
+                string outputPath = Path.Combine(Directory.GetCurrentDirectory(), $"output {timeStamp}.txt");
+
+                using (StreamWriter writer = new StreamWriter(outputPath))
+                {
+                    AssignmentReporter.WriteAssignments(writer, assignments, vehicles, jobs);
+                    AssignmentReporter.WriteUnassignedJobs(writer, unassignedJobs);
+                    AssignmentReporter.WriteUnassignedVehicles(writer, unassignedVehicles);
+                }
             }
             catch (FileNotFoundException ex)
             {
@@ -65,40 +69,6 @@ namespace TruckOrderOptimizer
                 throw new FileNotFoundException($"The specified file {fileName} does not exist or is empty");
             }
             return path;
-        }
-
-        private static void PrintAssignments(Dictionary<int, int> assignments, List<Vehicle> vehicles, List<Job> jobs)
-        {
-            Log.Information("Number of assignments made: {AssignmentsCount}", assignments.Count);
-
-            foreach (var assignment in assignments)
-            {
-                var vehicle = vehicles.FirstOrDefault(v => v.Id == assignment.Value);
-                var job = jobs.FirstOrDefault(j => j.Id == assignment.Key);
-
-                if (vehicle != null && job != null)
-                {
-                    Log.Information("Vehicle {VehicleId} ({Join}) -> Job {JobId} ({JobType})", vehicle.Id, string.Join(" ", vehicle.CompatibleJobs), job.Id, job.Type);
-                }
-            }
-        }
-
-        private static void PrintUnassignedJobs(List<Job> unassignedJobs)
-        {
-            Log.Information("Unassigned Jobs:");
-            foreach (var unassignedJob in unassignedJobs)
-            {
-                Log.Information($"Job {unassignedJob.Id} ({unassignedJob.Type})");
-            }
-        }
-
-        private static void PrintUnassignedVehicles(List<Vehicle> unassignedVehicles)
-        {
-            Log.Information("Unassigned Vehicles:");
-            foreach (var unassignedVehicle in unassignedVehicles)
-            {
-                Log.Information($"Vehicle {unassignedVehicle.Id} ({string.Join(" ", unassignedVehicle.CompatibleJobs)})");
-            }
         }
     }
 }
