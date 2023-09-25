43dbd4948281c9851e5163df22e2d305b0b14ae6
GEOMANT\fjancsar <fjancsar@geomant.com>
2023-09-25 19:16:26 +0200

    Added serilog and logging in stead of prints, added stopwatch out of curiosity

--- TruckOrderOptimizer/Helpers/FileParser.cs
+++ TruckOrderOptimizer/Helpers/FileParser.cs
@@ -1,24 +1,28 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
+using Serilog;
+using System.Linq;
 using TruckOrderOptimizer.Models;
 
 namespace TruckOrderOptimizer.Helpers
 {
     public static class FileParser
     {
-        public static void ParseTrucksAndJobs(string filepath, out List<Vehicle> vehicles, out List<Job> jobs)
+        public static (List<Vehicle> Vehicles, List<Job> Jobs) ParseTrucksAndJobs(string filepath)
         {
-            vehicles = new List<Vehicle>();
-            jobs = new List<Job>();
+            var vehicles = new List<Vehicle>();
+            var jobs = new List<Job>();
 
-            using (var reader = new StreamReader(filepath))
+            try
             {
+                using var reader = new StreamReader(filepath);
+
                 int vehicleCount = int.Parse(reader.ReadLine());
                 for (int i = 0; i < vehicleCount; i++)
                 {
                     string[] tokens = reader.ReadLine().Split(' ');
                     int vehicleId = int.Parse(tokens[0]);
-                    var compatibleJobs = new List<string>(tokens).GetRange(1, tokens.Length - 1);
                     var compatibleJobs = new HashSet<string>(tokens.Skip(1));
                     vehicles.Add(new Vehicle { Id = vehicleId, CompatibleJobs = compatibleJobs });
                 }
@@ -31,6 +35,23 @@ namespace TruckOrderOptimizer.Helpers
                     string jobType = tokens[1];
                     jobs.Add(new Job { Id = jobId, Type = jobType });
                 }
+
+                return (vehicles, jobs);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Log.Error("File not found: {ExFileName}", ex.FileName);
+                throw;
+            }
+            catch (FormatException ex)
+            {
+                Log.Error("Invalid file format: {ExMessage}", ex.Message);
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Log.Error("An error occurred: {ExMessage}", ex.Message);
+                throw;
             }
         }
     }
--- TruckOrderOptimizer/Program.cs
+++ TruckOrderOptimizer/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using TruckOrderOptimizer.Helpers;
 using TruckOrderOptimizer.Services;
+using Serilog;
+using TruckOrderOptimizer.Models;
 
 namespace TruckOrderOptimizer
 {
@@ -10,34 +14,90 @@ namespace TruckOrderOptimizer
     {
         public static void Main()
         {
-            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "mediordev.txt");
-            FileParser.ParseTrucksAndJobs(filePath, out var vehicles, out var jobs);
+            ConfigureLogging();
 
-            var (assignments, unassignedJobs, unassignedVehicles) = JobAssignmentService.AssignJobs(vehicles, jobs);
+            try
+            {
+                var filePath = ResolveFilePath("mediordev.txt");
 
-            // Printing the assignments
+                // Start the stopwatch
+                var stopwatch = Stopwatch.StartNew();
+
+                var (vehicles, jobs) = FileParser.ParseTrucksAndJobs(filePath);
+                var (assignments, unassignedJobs, unassignedVehicles) = JobAssignmentService.AssignJobs(vehicles, jobs);
+
+                // Stop the stopwatch
+                stopwatch.Stop();
+
+                // Log the elapsed time
+                Log.Information("Elapsed time: {ElapsedMilliseconds} ms", stopwatch.ElapsedMilliseconds);
+
+                // PrintAssignments(assignments, vehicles, jobs);
+                // PrintUnassignedJobs(unassignedJobs);
+                // PrintUnassignedVehicles(unassignedVehicles);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Log.Error(ex, "The specified file could not be found");
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "An unhandled exception occurred");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static void ConfigureLogging()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+
+        private static string ResolveFilePath(string fileName)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+            if (!File.Exists(path) || new FileInfo(path).Length == 0)
+            {
+                throw new FileNotFoundException($"The specified file {fileName} does not exist or is empty");
+            }
+            return path;
+        }
+
+        private static void PrintAssignments(Dictionary<int, int> assignments, List<Vehicle> vehicles, List<Job> jobs)
+        {
+            Log.Information("Number of assignments made: {AssignmentsCount}", assignments.Count);
+
             foreach (var assignment in assignments)
             {
-                var vehicle = vehicles.First(v => v.Id == assignment.Value);
-                var job = jobs.First(j => j.Id == assignment.Key);
-                Console.WriteLine($"Vehicle {vehicle.Id} ({string.Join(" ", vehicle.CompatibleJobs)}) -> Job {job.Id} ({job.Type})");
+                var vehicle = vehicles.FirstOrDefault(v => v.Id == assignment.Value);
+                var job = jobs.FirstOrDefault(j => j.Id == assignment.Key);
+
+                if (vehicle != null && job != null)
+                {
+                    Log.Information("Vehicle {VehicleId} ({Join}) -> Job {JobId} ({JobType})", vehicle.Id, string.Join(" ", vehicle.CompatibleJobs), job.Id, job.Type);
+                }
             }
+        }
 
-            Console.WriteLine(($"Number of assignments made: {assignments.Count}"));
-
-            
-            // Printing the unassigned jobs
-            Console.WriteLine("Unassigned Jobs:");
+        private static void PrintUnassignedJobs(List<Job> unassignedJobs)
+        {
+            Log.Information("Unassigned Jobs:");
             foreach (var unassignedJob in unassignedJobs)
             {
-                Console.WriteLine($"Job {unassignedJob.Id} ({unassignedJob.Type})");
+                Log.Information($"Job {unassignedJob.Id} ({unassignedJob.Type})");
             }
+        }
 
-            // Printing the unassigned vehicles
-            Console.WriteLine("Unassigned Vehicles:");
+        private static void PrintUnassignedVehicles(List<Vehicle> unassignedVehicles)
+        {
+            Log.Information("Unassigned Vehicles:");
             foreach (var unassignedVehicle in unassignedVehicles)
             {
-                Console.WriteLine($"Vehicle {unassignedVehicle.Id} ({string.Join(" ", unassignedVehicle.CompatibleJobs)})");
+                Log.Information($"Vehicle {unassignedVehicle.Id} ({string.Join(" ", unassignedVehicle.CompatibleJobs)})");
             }
         }
     }
